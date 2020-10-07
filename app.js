b789aa1b5d66b98d876a8132a4f1169bd09e9145
Ankur Gupta <ankurnecessary@gmail.com>
2020-10-07 08:02:31 +0530

    lecture30: Sending Responses

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const http = require('http');
 // Server creation
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
+  res.setHeader('Content-type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>This is my first page</title></head>');
+  res.write('<body><h1>This is a page from nodejs </h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
 // Start the server for persistant listening of requests
