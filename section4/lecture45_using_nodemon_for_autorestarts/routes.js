a923ec90ba79021b8cd772ca8c948f7c629b050a
Ankur Gupta <ankurnecessary@gmail.com>
2020-11-29 13:22:53 +0530

    Lecture52: Restarting the debugger automatically after editing our app

--- section4/lecture45_using_nodemon_for_autorestarts/routes.js
+++ section4/lecture45_using_nodemon_for_autorestarts/routes.js
@@ -33,14 +33,14 @@ const requestHandler = (req, res) => {
       });
     });
   }
-
+  
   res.setHeader('Content-type', 'text/html');
   res.write('<html>');
   res.write('<head><title>This is my first page</title></head>');
   res.write('<body><h1>This is a page from nodejs </h1></body>');
   res.write('</html>');
   res.end();
-  
+
 }
 
 module.exports = requestHandler;
