24901df4df1d6eae667e3aa823c5f03a1c3cfbb4
Ankur Gupta <ankurnecessary@gmail.com>
2021-01-02 17:20:09 +0530

    Lecture 68: Filtering routes

---
+++ section5/lecture68_filtering_paths/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+// Intializing an express app
+const app = express();
+
+// A middleware to handle form post requests
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Setting routes for administrator
+app.use('/admin', adminRoutes);
+
+// Setting routes for the shoppers
+app.use(shopRoutes);
+
+// For setting 404 page not found for unknown routes
+// app.use('/',(req, res, next) => { We could have used this
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>');
+});
+
+// Server creation
+app.listen(3000);
