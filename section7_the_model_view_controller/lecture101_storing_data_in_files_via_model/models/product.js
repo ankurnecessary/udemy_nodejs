203513bfe4f68f8cb8964a1bd2d42ff34c7197ad
Ankur Gupta <ankurnecessary@gmail.com>
2021-04-26 07:40:05 +0530

    Lecture 101: Storing data in files via model_has error

---
+++ section7_the_model_view_controller/lecture101_storing_data_in_files_via_model/models/product.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const path = require('path');
+
+// Path to products.json file
+const productPath = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+module.exports = class Product {
+  constructor(title) {
+    this.title = title;
+  }
+
+  /**
+   * To add products
+   */
+  save() {
+
+    // Reading existing products in the file products.json
+    fs.readFile(productPath, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      
+      // Pushing new product inside the array
+      products.push(this);
+
+      // Writing updated product data to products.json
+      fs.writeFile(productPath, JSON.stringify(products), (err) => {
+        console.log(err);
+      })
+
+    });
+
+  }
+
+  /**
+   * To get the list of all the products
+   */
+  static fetchAll() {
+    fs.readFile(productPath, (err, fileContent) => { 
+      
+      if(err) return [];
+      return JSON.parse(fileContent);
+
+    });
+  }
+}
